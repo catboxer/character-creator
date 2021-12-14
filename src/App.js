be84271f34bcf87906ab43b2ab3173dff52eac7f
Rosalee Rester <70821412+catboxer@users.noreply.github.com>
2021-12-14 06:50:38 -0800

    add files via upload
    src

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import CharacterEditor from './components/CharacterEditor';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+  
+    <div>
+    <CharacterEditor />
+      <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
